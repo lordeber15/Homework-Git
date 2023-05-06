63fda3fc060bfcbd4eb9ec85ecb19c08ba56727c
Facundovila03 <facundovila03@gmail.com>
2023-05-06 17:03:13 -0300

    function name

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function laCajaDePandora(numero) {
   const colision = true;
   return numero % 2 ? numero.toString(16) : numero.toString(2);
 }
+
+function facu() {
+  return {
+    nombre: "Facundo",
+    edad: 19,
+    nacionalidad: "Argentino",
+  };
+}
