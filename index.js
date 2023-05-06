cde5b9a0619445fb718fdccc34b4d6b7417e813e
jesusudiz <jesusgabrieludiz@gmail.com>
2023-05-06 16:02:01 -0400

    homework de jesus

---
+++ index.js
@@ -0,0 +1,13 @@
+function laCajaDePandora(numero){
+    // proximamente escribiremos codigo aqui
+    return numero % 2 === 0? numero.toString(2):numero.toString(16)
+    }
+
+
+function jesus (){
+    return {
+        nombre:"jesus",
+        edad:35,
+        nacionalidad: "venezolano"
+    }
+}
